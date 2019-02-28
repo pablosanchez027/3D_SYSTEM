2336061e8a9d26b54b7be8f01e518cd1e598be04
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-02-28 14:24:48 -0700

    Practica

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement.Sys3D;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)]
+    float moveSpeed = 0f;
+
+    void Update()
+    {
+        Movement3D.DeltaMovement(transform, moveSpeed);
+    }
+}
